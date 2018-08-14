046272c842942bcc23a7d9a38c72470fb27e46e2
priyanka <priyankareddy1885@gmail.com>
2018-08-14 06:10:26 -0700

    CCI string compress

---
+++ 1.5_string_compress.py
@@ -0,0 +1,21 @@
+## problem: Implement a method to perform basic string compression using the counts of repeated characters.
+def sring_compression(string):
+    res = ""
+    count = 1
+    #Add in first character
+    res += string[0]
+    #Iterate through loop, skipping last one
+    for i in range(len(string)-1):
+        if(string[i] == string[i+1]):
+            count+=1
+        else:
+            if(count >= 1):
+                #Ignore if no repeats
+                res += str(count)
+            res += string[i+1]
+            count = 1
+    #print last one
+    if(count >= 1):
+        res += str(count)
+    return res
+
